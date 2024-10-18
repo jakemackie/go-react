312fb302eeae7a6a36443c99306caf656a6ba392
Jake Mackie <jake@hiyield.co.uk>
2024-10-18 08:36:53 +0100

    amend: minor - better scalability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,9 +18,9 @@ function App() {
   const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher)
 
   return (
-    <div className="mx-auto min-h-screen max-w-lg flex flex-col justify-center items-center px-4 lg:px-0 gap-12">
+    <div className="mx-auto min-h-screen max-w-lg flex flex-col items-center p-4 gap-12">
 
-      <div className="w-full flex flex-col gap-4">
+      <div className="lg:mt-8 w-full flex flex-col gap-4">
         <Alert>
           <AlertTitle>Heads up!</AlertTitle>
           <AlertDescription>
@@ -31,7 +31,7 @@ function App() {
       </div>
       
       {data && data.length > 0 && (
-      <div className="w-full lg:fixed lg:bottom-4 lg:right-4 lg:w-80 space-y-4">
+      <div className="w-full lg:fixed lg:bottom-4 lg:right-4 lg:max-w-lg space-y-4">
         <h2 className="text-2xl">API Data</h2>
         <ul className="max-h-56 w-full overflow-y-scroll flex flex-col p-4 bg-zinc-900 rounded-2xl ring-4 ring-zinc-800">
         {data.map((todo) => (
