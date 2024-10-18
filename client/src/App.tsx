3ed7f643e7d6f659fb51b65c99f6fe357c60d20d
Jake Mackie <jake@hiyield.co.uk>
2024-10-18 08:13:35 +0100

    amend: Mobile view bc why not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,30 +18,28 @@ function App() {
   const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher)
 
   return (
-    <div className="mx-auto min-h-screen max-w-sm flex flex-col justify-center items-center gap-4">
+    <div className="mx-auto min-h-screen max-w-lg flex flex-col justify-center items-center px-4 lg:px-0 gap-12">
 
-      <div className="w-full">
+      <div className="w-full flex flex-col gap-4">
         <Alert>
           <AlertTitle>Heads up!</AlertTitle>
           <AlertDescription>
-            Any entries added are only stored in memory and will be lost when the server is restarted.
+          Any entries added are only stored in memory and will be lost when the server is restarted.
           </AlertDescription>
         </Alert>
-      </div>
-
-      <div className="w-full">
         <AddTodo mutate={mutate} />
       </div>
       
       {data && data.length > 0 && (
-      <div className="fixed bottom-0 right-0 m-4 max-h-56 w-80 overflow-y-scroll rounded-2xl ring-4 ring-zinc-800">
-          <ul className="w-full p-4 flex flex-col bg-zinc-900 ">
-            {data.map((todo) => (
-              <li key={todo.id}>
-          <pre>{JSON.stringify(todo, null, 2)}</pre>
-              </li>
-            ))}
-          </ul>
+      <div className="w-full lg:fixed lg:bottom-4 lg:right-4 lg:w-80 space-y-4">
+        <h2 className="text-2xl">API Data</h2>
+        <ul className="max-h-56 w-full overflow-y-scroll flex flex-col p-4 bg-zinc-900 rounded-2xl ring-4 ring-zinc-800">
+        {data.map((todo) => (
+          <li key={todo.id}>
+        <pre>{JSON.stringify(todo, null, 2)}</pre>
+          </li>
+        ))}
+        </ul>
       </div>
       )}
 
