ffb5846735a296691a7a5df164d7b4b66c8c9a77
Jake Mackie <jake@hiyield.co.uk>
2024-10-18 07:59:22 +0100

    feat: A few ui changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import useSWR from "swr"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import AddTodo from "./components/addTodo"
 
 export interface Todo {
@@ -17,22 +18,33 @@ function App() {
   const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher)
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
-      <div className="mx-auto w-full max-w-xl flex gap-8">
-        <div className="w-1/2 max-h-md">
-          <AddTodo mutate={mutate} />
-        </div>
-        <div className="w-1/2 max-h-56 overflow-y-scroll">
-          <ul className="w-full py-4 px-3 rounded-lg flex flex-col gap-4 bg-gray-50 shadow-lg">
-            {data?.map((todo) => (
-              <li key={todo.id} className="flex flex-col gap-2 bg-green-500 text-white p-4 rounded-xl">
-                <h2 className="text-lg font-bold">{todo.title}</h2>
-                <p>{todo.body}</p>
+    <div className="mx-auto min-h-screen max-w-sm flex flex-col justify-center items-center gap-4">
+
+      <div className="w-full">
+        <Alert>
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            Any entries added are only stored in memory and will be lost when the server is restarted.
+          </AlertDescription>
+        </Alert>
+      </div>
+
+      <div className="w-full">
+        <AddTodo mutate={mutate} />
+      </div>
+      
+      {data && data.length > 0 && (
+      <div className="fixed bottom-0 right-0 m-4 max-h-56 w-80 overflow-y-scroll rounded-2xl ring-4 ring-zinc-800">
+          <ul className="w-full p-4 flex flex-col bg-zinc-900 ">
+            {data.map((todo) => (
+              <li key={todo.id}>
+          <pre>{JSON.stringify(todo, null, 2)}</pre>
               </li>
             ))}
           </ul>
-        </div>
       </div>
+      )}
+
     </div>
   )
 }
